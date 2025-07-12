5edf8c3235a9a89e6e1cc19a9de5114db99e7c92
Orest <124895227+OrestPokotylenko@users.noreply.github.com>
2025-07-12 22:12:01 +0200

    implement port-controller

--- stream-deck-client/Program.cs
+++ stream-deck-client/Program.cs
@@ -1,13 +1,15 @@
 ﻿namespace stream_deck_client
 {
-    using System;
-    using System.IO.Ports;
+    using utility;
 
     internal class Program
     {
         static void Main(string[] args)
         {
-
+            PortController portController = new();
+            portController.InitPort();
+            portController.WriteSong("Test", "3:29");
+            portController.ClosePort();
         }
     }
 }
---
+++ stream-deck-client/utility/PortController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO.Ports;
+
+namespace stream_deck_client.utility
+{
+    public class PortController
+    {
+        private static SerialPort port;
+
+        public void InitPort()
+        {
+            string songName = "Stan";
+
+            port = new("COM3", 115200);
+            port.Open();
+
+            Console.WriteLine("Sending data...");
+            port.WriteLine(songName);
+            port.WriteLine("4:15");
+            Console.WriteLine("Data sent. Waiting for response...");
+        }
+
+        public void ClosePort()
+        {
+            port.Close();
+        }
+
+        private void WriteToPort(string data)
+        {
+            port.WriteLine(data);
+        }
+
+        public void WriteSong(string name, string duration)
+        {
+            WriteToPort(name);
+            WriteToPort(duration);
+        }
+    }
+}
