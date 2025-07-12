9e98aaf8a057982a66ad1b7607e2f986200617c6
Orest <124895227+OrestPokotylenko@users.noreply.github.com>
2025-07-12 21:19:28 +0200

    initial commit

---
+++ stream-deck-client/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace stream_deck_client
+{
+    using System;
+    using System.IO.Ports;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
